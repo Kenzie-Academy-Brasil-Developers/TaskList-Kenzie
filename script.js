9b0961180b874f591d2af26a22a78ce5ef994a4f
Gustavo <honoratogustavo7@gmail.com>
2023-10-05 17:25:26 -0300

    ajustes de estilização e tarefa 2

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+function renderElements(tasks){
+  const list = document.querySelector('ul')
+  for (let i = 0; i < tasks.length; i++){
+    let teste = createTaskItem(tasks[i])
+    list.appendChild(teste)
+
+  }
+
+}
+
+function createTaskItem(taskList){
+  // criando os elementos
+  const list = document.createElement('li')
+  const infoContainer = document.createElement('div')
+  const taskType = document.createElement('span')
+  const taskTittle = document.createElement('p')
+  const deleteButton = document.createElement('button')
+  //classificando
+  list.classList.add("task__item")
+  infoContainer.classList.add("task-info__container")
+  taskType.classList.add("task-type")
+  if(taskList.type == 'Urgente' ){
+  taskType.classList.add('span-urgent')  
+  } else if (taskList.type == 'Importante'){
+  taskType.classList.add('span-important')
+  } else if (taskList.type == 'Normal'){
+    taskType.classList.add('span-normal') 
+  }
+  taskTittle.innerText = taskList.title
+  deleteButton.classList.add("task__button--remove-task")
+
+  //hieraquia
+  list.append(infoContainer, deleteButton)
+  infoContainer.append(taskType, taskTittle)
+  
+  return list
+
+
+}
+renderElements(tasks)
